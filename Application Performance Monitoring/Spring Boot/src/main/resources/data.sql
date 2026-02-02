ce0066134359e645db3fb391497e2c80792dc5ea
watarunS <116529775+watarunS@users.noreply.github.com>
2026-02-02 13:50:56 +0900

    Delete Application Performance Monitoring/Spring Boot directory

--- Application Performance Monitoring/Spring Boot/src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO product (name, price, description) VALUES
-('Laptop', 1200, '13-inch display'),
-('Mouse', 30, 'Wireless'),
-('Keyboard', 50, 'US layout'),
-('Monitor', 300, '27-inch 4K');
-
